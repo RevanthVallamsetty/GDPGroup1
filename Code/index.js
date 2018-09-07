0a0e58f6f6ec3d7339b7a0a25eae93eb7033d5de
RevanthVallamsetty <33847680+RevanthVallamsetty@users.noreply.github.com>
2018-09-07 18:31:46 -0500

    installed express

---
+++ Code/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+
+const DEFAULT_PORT = 8089
+
+const app = express()
+
+// configure app.settings.............................
+app.set('port', process.env.PORT || DEFAULT_PORT)
+
+// set the root view folder
+app.set('views', path.join(__dirname, 'views'))
+
+// specify desired view engine
+app.set('view engine', 'ejs')
+app.engine('ejs', engines.ejs)
+
+// start Express app
+app.listen(app.get('port'), () => {
+    console.log('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'))
+    console.log('  Press CTRL-C to stop\n')
+   })
+   
+   module.exports = app
